e4de4ca071c8604c2ddfec218727b16ddb436d6f
Harumi <haruumi.nishimura@gmail.com>
2020-04-29 14:41:46 -0300

    projeto react em branco adicionado ao repositrio

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nosso futuro projeto em React.
         </p>
         <a
           className="App-link"
