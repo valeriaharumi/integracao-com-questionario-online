0307412a66e55eb380ed46ad864132cbecaf4d3a
Harumi <haruumi.nishimura@gmail.com>
2020-05-07 15:10:06 -0300

    adicionado o router-dom

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header'
 import Footer from './components/Footer';
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
 
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
   <div>    
     <Header/>
-    
+      <BrowserRouter>
+        <Switch>
+          {/*<Route path="/" exact={true} component={Home}/>*/}
+          {/*<Route path="/sobre" component={Sobre}/>*/}
+          {/*<Route path="/login" component={Login}/>*/}
+        </Switch>
+      </BrowserRouter>
     <Footer/>
   </div>  
   
