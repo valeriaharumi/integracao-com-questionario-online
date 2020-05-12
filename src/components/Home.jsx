9d2edccf4c336dd75711f7d408f910bc2fdd1433
WesleyCamara <wesleycamara@hotmail.com.br>
2020-05-11 22:26:43 -0300

    realizados ajustes na captura do formulário

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
 
     // Faz o chamado post passando como body os dados preenchidos na tabela
     const postData = (body) => {
-         axios.post("https://api.airtable.com/v0/appYtQmjCS4p0n2dY/Question%C3%A1rio?api_key=keyfV0AwOq2Pctb5Y", body
-    )
+        axios.post("https://api.airtable.com/v0/appYtQmjCS4p0n2dY/Question%C3%A1rio?api_key=keyfV0AwOq2Pctb5Y", body
+        )
     }
 
     // captura as informações do input
@@ -22,25 +22,16 @@ const Home = () => {
         email: "",
         phone: "",
         subject: "",
-        msgContent: ""
+        msgContent: "",
+        contatoTelefone: "",
+        contatoEmail: "",
+        comercial: "",
+        manha: "",
+        tarde: "",
+        noite: "",
+        outros: ""
+
     });
-
-
-    // Quando detecta mudança no input atualiza automaticamente dados da const form 
-    function handleChange(event) {
-        setForm({
-            ...form,
-            [event.target.id]: event.target.value
-        });
-
-    }
-
-
-    // funcação de apoio para TextDecoderStream, foi criar pra não acionar o submit do form 
-    function clicado() {
-        console.log('clicou', data.records[0].fields)
-        cadastrar()
-    }
 
     // constante com o conteúdo que será enviado pela API 
     const data = {
@@ -52,40 +43,50 @@ const Home = () => {
                     "Telefone": form.phone,
                     "Assunto": form.subject,
                     "Conteúdo": form.msgContent,
-                    "Forma de contato": "",
-                    "Disponibilidade para contato": ""
+                    "Forma de contato": form.contato,
+                    "Disponibilidade para contato": form.comercial
                 }
             }
         ]
     }
 
+
+    // Quando detecta mudança no input atualiza automaticamente dados da const form 
+    function handleChange(event) {
+        setForm({
+            ...form,
+            [event.target.id]: event.target.value
+        });
+
+    }
+
     // Auxiliar para preenchimento da funçao data, informações opcionais 
     const auxDisponibilidade = {
-        "Comercial": false,
-        "Manha": false,
-        "Tarde": false,
-        "Noite": false,
-        "Outros": ""
+        "comercial": form.comercial,
+        "manha": form.manha,
+        "tarde": form.tarde,
+        "noite": form.noite,
+        "outros": ""
     }
 
     // Função acionada quando o formulário e enviado, registra as informações de contato e chama as seguintes 
-    function cadastrar(){
+    function cadastrar() {
         let aux = []
 
-        if (auxDisponibilidade["Comercial"]) {
+        if (auxDisponibilidade["comercial"]) {
             aux.push("comercial")
         }
-        if (auxDisponibilidade["Manha"]) {
+        if (auxDisponibilidade["manha"]) {
             aux.push("manha")
         }
-        if (auxDisponibilidade["Tarde"]) {
+        if (auxDisponibilidade["tarde"]) {
             aux.push("tarde")
         }
-        if (auxDisponibilidade["Noite"]) {
+        if (auxDisponibilidade["noite"]) {
             aux.push("noite")
         }
-        if (auxDisponibilidade["Outros"] != "") {
-            aux.push(this.auxDisponibilidade["Outros"]);
+        if (auxDisponibilidade["outros"] != "") {
+            aux.push(auxDisponibilidade["outros"]);
         }
 
         let disponibilidade = "";
@@ -96,12 +97,18 @@ const Home = () => {
 
         data.records[0].fields["Disponibilidade para contato"] = disponibilidade;
         enviado = true;
-
+        console.log('data enviar', data.records[0].fields)
     }
 
 
+    // funcação de apoio para teste, foi criar pra não acionar o submit do form 
+    function clicado() {
+        console.log("form", form)
+        cadastrar()
+    }
 
-// -----------Renderização da página---------------
+
+    // -----------Renderização da página---------------
 
     return <div id="page">
 
@@ -113,22 +120,10 @@ const Home = () => {
 
                 <hr></hr>
 
-
-        {/* Auxilio para teste, será excluido depois  */}
-                <ul>
-                    <li>Nome: {form.name}</li>
-                    <li>Email: {form.email}</li>
-                    <li>telefone: {form.phone}</li>
-                    <li>assunto: {form.subject}</li>
-                    <li>msgContent: {form.msgContent}</li>
-
-                </ul>
-
-
                 <div className="message"></div>
                 <div className="message"></div>
 
-                <div onClick={clicado}>teste</div>
+
                 <form name="form-Index" id="form-Index">
                     <div className="row">
 
@@ -151,10 +146,7 @@ const Home = () => {
                                     className="form-control"
                                     type="text"
                                     name="name" id="name"
-
                                     minLength="3" placeholder="João da Silva" required />
-
-
 
 
                                 {/* <div className="alert alert-danger">
@@ -184,8 +176,6 @@ const Home = () => {
                                     placeholder="email@email.com"
                                     title="Preencha o campo no seguinte modelo: email@dominio.com" required="required"
                                     pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
-
-
 
                                 {/* <div className="alert alert-danger">
                     <div >
@@ -256,16 +246,12 @@ const Home = () => {
                                     maxLength="500"
                                     placeholder="Digite aqui sua mensagem" />
 
-
-
-
                             </div>
 
 
                         </div>
 
                         {/* --------------------------------- AREA DE INFORMAÇÕES DE CONTATO----------------------- */}
-
 
                         <div className="col-sm-12 col-md-12 col-lg-6">
 
@@ -279,71 +265,98 @@ const Home = () => {
 
                                 <div className="form-check">
                                     <input
+                                        onChange={handleChange}
                                         type="radio"
                                         name="conta"
-                                        id="email-contact"
+                                        id="contatoEmail"
                                         className="form-check-input"
-                                        value="Email" />
-                                    <label htmlFor="email-contact">Email</label>
+                                        value="contatoEmail" />
+                                    <label
+                                        htmlFor="contatoEmail">Email</label>
                                 </div>
 
                                 <div className="form-check">
                                     <input
+                                        onChange={handleChange}
                                         type="radio"
                                         name="conta"
-                                        id="tel-contact"
+                                        id="contatoTelefone"
                                         className="form-check-input"
-                                        value="Telefone" />
-                                    <label htmlFor="tel-contact">Telefone</label>
+                                        value="contatoTelefone" />
+                                    <label
+                                        htmlFor="contatoTelefone">Telefone</label>
                                 </div>
 
                                 {/* -----------------------------------Disponibilidade, tem caixas checkbox --> */}
                                 <h3>Disponibilidade para contato:</h3>
 
                                 <div className="form-check">
-                                    <input className="form-check-input"
+                                    <input
+                                        onChange={handleChange}
+                                        className="form-check-input"
                                         type="checkbox"
                                         name="disp1"
-                                        id="input-comercial"
+                                        id="comercial"
                                         value="comercial" />
-                                    <label className="form-check-label" htmlFor="input-comercial">
+                                    <label
+                                        className="form-check-label"
+                                        htmlFor="comercial">
                                         Seg. a Sexta - horário comercial</label>
                                 </div>
 
                                 <div className="form-check">
-                                    <input className="form-check-input"
+                                    <input
+                                        onChange={handleChange}
+                                        className="form-check-input"
                                         type="checkbox"
                                         name="disp2"
-                                        id="input-manha"
+                                        id="manha"
                                         value="manha" />
                                     <label className="form-check-label"
-                                        htmlFor="input-manha">
+                                        htmlFor="manha">
                                         Seg. a Sexta - Período da manhã</label>
                                 </div>
 
                                 <div className="form-check">
-                                    <input className="form-check-input"
+                                    <input
+                                        onChange={handleChange}
+                                        className="form-check-input"
                                         type="checkbox"
                                         name="disp3"
-                                        id="input-tarde"
+                                        id="tarde"
                                         value="tarde" />
-                                    <label className="form-check-label"
-                                        htmlFor="input-tarde">
+                                    <label
+                                        className="form-check-label"
+                                        htmlFor="tarde">
                                         Seg. a Sexta - Período da tarde</label>
                                 </div>
 
                                 <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" name="disp4"
-                                        id="input-noite" value="noite">
+                                    <input
+                                        onChange={handleChange}
+                                        className="form-check-input"
+                                        type="checkbox"
+                                        name="disp4"
+                                        id="noite"
+                                        value="noite">
                                     </input>
-                                    <label className="form-check-label" htmlFor="input-noite">
+                                    <label
+                                        className="form-check-label"
+                                        htmlFor="noite">
                                         Seg. a Sexta - Período da noite</label>
                                 </div>
 
                                 <div className="form-check">
-                                    <div className="form-others">
-                                        <label htmlFor="text-input" className="form-check-label">Outros:</label>
-                                        <input className="form-control" type="text" name="disp5"
+                                    <div
+                                        className="form-others">
+                                        <label
+                                            htmlFor="text-input"
+                                            className="form-check-label">
+                                            Outros:</label>
+                                        <input
+                                            className="form-control"
+                                            type="text"
+                                            name="disp5"
                                             id="text-input">
                                         </input>
                                     </div>
@@ -353,14 +366,16 @@ const Home = () => {
 
                             {/* -----------------------------------Botoes para enviar e cancelar --> */}
                             <div id="buttons">
+
+                                <div onClick={clicado}>TESTE</div>
+
                                 <button type="reset"
                                     className="btn cancel--button">Cancelar
-            </button>
+                                 </button>
 
                                 <button type="submit"
                                     className="btn send--button" >enviar
-            </button>
-
+                                </button>
                             </div>
 
 
@@ -368,10 +383,6 @@ const Home = () => {
 
                     </div>
                 </form>
-
-
-
-
 
             </div>
         </div>
