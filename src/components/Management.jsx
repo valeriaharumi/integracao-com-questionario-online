296cc9be72bd662cabeb6bc906ebefda8dcc5f9a
WesleyCamara <wesleycamara@hotmail.com.br>
2020-05-13 19:55:01 -0300

    Corrigido erro de estilo

--- src/components/Management.jsx
+++ src/components/Management.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
-import '../css/management.css'
+
 import '../assets/bootstrap/bootstrap.min.css'
+import '../css/management.css'
 
 class Management extends Component {
     render() {
         return (
-            <div className="container">
+            <div className="container management">
                 <div className="title">
                     <h3>Gerenciamento de Respostas</h3>
                 </div>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import Home from './components/Home';
 import Sobre from './components/Sobre'
 import Login from './components/Login';
+import Management from './components/Management'
 
 
 
@@ -19,6 +20,8 @@ ReactDOM.render(
           <Route path="/" exact={true} component={Home}/>
           <Route path="/sobre" component={Sobre}/>
           <Route path="/login" component={Login}/>
+          <Route path="/logado" component={Management}/>
+
         </Switch>
       </BrowserRouter>
     <Footer/>
