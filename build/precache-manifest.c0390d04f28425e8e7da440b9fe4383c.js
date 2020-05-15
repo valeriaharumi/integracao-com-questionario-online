88a98f917ab9a25536c133dabf4d84408c5f5522
valéria harumi <61988053+valeriaharumi@users.noreply.github.com>
2020-05-15 18:38:24 -0300

    Add files via upload

---
+++ build/precache-manifest.c0390d04f28425e8e7da440b9fe4383c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b3db7cc3681fd7403b5bc771a0b34d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2dadfdd2e9c319590e6",
+    "url": "/static/css/main.b63fb17d.chunk.css"
+  },
+  {
+    "revision": "6d4cfa9626fae4dadfdc",
+    "url": "/static/js/2.d5276370.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d5276370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2dadfdd2e9c319590e6",
+    "url": "/static/js/main.fe1c4112.chunk.js"
+  },
+  {
+    "revision": "f10ee6bc68a8169bd685",
+    "url": "/static/js/runtime-main.926580ec.js"
+  },
+  {
+    "revision": "77fbff5f567c5aa2138a11e223c7945f",
+    "url": "/static/media/benicio.77fbff5f.jpg"
+  },
+  {
+    "revision": "02bd0321dc14bcdeb89ae5c061b6cf2f",
+    "url": "/static/media/carlos.02bd0321.jpg"
+  },
+  {
+    "revision": "c76da18cfab1962a26f014c7416a5007",
+    "url": "/static/media/fabricio.c76da18c.jpg"
+  },
+  {
+    "revision": "52e03bef23ff041055903c2e42f2ead3",
+    "url": "/static/media/opensans-bold-webfont.52e03bef.woff2"
+  },
+  {
+    "revision": "72c3b5eebd9e989503ac2ac337f39225",
+    "url": "/static/media/opensans-bold-webfont.72c3b5ee.woff"
+  },
+  {
+    "revision": "7124c907ad59be8464bdbf2d313931b3",
+    "url": "/static/media/opensans-extrabold-webfont.7124c907.woff"
+  },
+  {
+    "revision": "f7beae01cb4a162e36da21e116e849a9",
+    "url": "/static/media/opensans-extrabold-webfont.f7beae01.woff2"
+  },
+  {
+    "revision": "bdcc7c2e96f4d19caec47addc8a425a8",
+    "url": "/static/media/opensans-semibold-webfont.bdcc7c2e.woff2"
+  },
+  {
+    "revision": "f93f145d708f132b79e2c9f1d9958180",
+    "url": "/static/media/opensans-semibold-webfont.f93f145d.woff"
+  },
+  {
+    "revision": "dbb8c33953c85d52f70c76a223db6dcb",
+    "url": "/static/media/valeria.dbb8c339.jpg"
+  },
+  {
+    "revision": "4c47844c91f17a3a1379d506c8ccd740",
+    "url": "/static/media/wesley.4c47844c.jpg"
+  }
+]);
